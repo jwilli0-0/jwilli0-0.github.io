ee8fbcb30e1bccd04b6531af8d927cdfb0e0b5b9
jwilli0-0 <jessicawilliamson0612@gmail.com>
2026-03-27 14:54:02 -0400

    yaya

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ function closeNav() {
   document.getElementById("main").style.marginLeft = "0";
   document.getElementById("menuIcon").style.opacity = "100";
 }
+
+var modal = document.getElementById("resumeModal");
+var btn = document.getElementById("btnResume");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
