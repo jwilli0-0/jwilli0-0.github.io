6af65f4a2e309fdb70e30ebeea95cdfbb01ee024
jwilli0-0 <jessicawilliamson0612@gmail.com>
2026-03-27 13:41:31 -0400

    happy days

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("menuIcon").style.opacity = "0";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("menuIcon").style.opacity = "100";
+}
