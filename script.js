79d51e767f0dcb1bf315730bcf482a8b9bcc6ac7
jwilli0-0 <jessicawilliamson0612@gmail.com>
2026-03-28 23:25:43 -0400

    subb

--- script.js
+++ script.js
@@ -13,18 +13,41 @@ function closeNav() {
 
 var modal = document.getElementById("resumeModal");
 var btn = document.getElementById("btnResume");
+
+var modal1 = document.getElementById("reCraftModal");
+var btn1 = document.getElementById("btnRecraft");
+
+var modal2 = document.getElementById("dvdModal");
+var btn2 = document.getElementById("btnDVD");
+
 var span = document.getElementsByClassName("close")[0];
+var span1 = document.getElementsByClassName("close1")[0];
+var span2 = document.getElementsByClassName("close2")[0];
 
 btn.onclick = function() {
   modal.style.display = "block";
+}
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+btn2.onclick = function() {
+  modal2.style.display = "block";
 }
 
 span.onclick = function() {
   modal.style.display = "none";
 }
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
 
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    modal1.style.display = "none";
+    modal2.style.display = "none";
   }
 }
